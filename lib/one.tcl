fd56df827c239b2aeea89d5cfc3680c4ed4fb008
Torben Brosten <torben@dekka.net>
2008-09-08 12:15:19 +0000

    updating query to handle more cases

--- lib/one.tcl
+++ lib/one.tcl
@@ -10,7 +10,7 @@ set admin_p [ad_permission_p $package_id admin]
 # get transaction log comments. 
 db_0or1row get_transaction_comments "select response_reason_text, avs_code
       from authorize_gateway_result_log 
-      where transaction_id = :transaction_id and amount = :amount and (substr(txn_attempted_type,1,9) = 'AUTH_ONLY' or substr(txn_attempted_type,1,12) = 'AUTH_CAPTURE')"
+      where transaction_id = :transaction_id and amount = :amount and (substr(txn_attempted_type,1,9) = 'AUTH_ONLY' or substr(txn_attempted_type,1,12) = 'AUTH_CAPTURE') and response_reason_text is not null and avs_code is not null limit 1"
 
 if { [info exists response_reason_text] && [info exists avs_code] } {
     # decode left most avs_code. Second character is card CVV2/CVC2/CID code response
